5db963a1292e004a6dd83887b8b3323efb327b41
Lorenz Liebhart <ll-162143@hs-weingarten.de>
2018-01-22 18:25:56 +0100

    attempt on turning towards goal

--- maze/src/maze.py
+++ maze/src/maze.py
@@ -2,7 +2,7 @@
 # import dependencies
 import rospy
 import time  # needed to sleep between every phase
-from numpy import isnan  # analyze laserdata
+import numpy as np
 from tf.transformations import euler_from_quaternion  # needed for conversion of position angles
 
 # import services
@@ -38,6 +38,8 @@ class MazeSolver:
             self.msg_pose = Pose()
             self.msg_laser = LaserScan()
             self.rate = rospy.Rate(1.5)  # set a publish rate of 1.5 Hz
+            self.memory = [0][0]  # remember visited points and movement decision
+            self.goal = [44.3, 10]  # goal position desired to be reached # TODO read from publisher
 
             # various constants incoming
             self.CHECK_STRAIGHT_LASER_DATA = 10
@@ -50,7 +52,7 @@ class MazeSolver:
 
             rospy.spin()  # maintain the service open.
         except KeyboardInterrupt:
-            print("Shutting down multi square mover...")
+            print("Shutting down maze runner...")
 
     def service_callback(self, p_service_msg):
         """
@@ -60,6 +62,9 @@ class MazeSolver:
         """
         rospy.loginfo("entered service_callback")
         rospy.logdebug("service callback message: '{}'".format(p_service_msg))
+
+        # write the target position  # TODO check data type
+        self.goal = (p_service_msg.x, p_service_msg.y)
 
         my_response = MazeSrvMsgResponse()
         my_response.success = self.move_robot(p_service_msg)
@@ -108,25 +113,46 @@ class MazeSolver:
                 # at this point check only number of CHECK_STRAIGHT_LASER_DATA elements from msg
                 compressed_msg = self.msg_laser[len(self.msg_laser / 2) - self.CHECK_STRAIGHT_LASER_DATA / 2:
                                                 len(self.msg_laser / 2) - self.CHECK_STRAIGHT_LASER_DATA / 2]
-                nan_count = len(filter(lambda x: isnan(x), compressed_msg))
+                nan_count = len(filter(lambda x: np.isnan(x), compressed_msg))
 
                 if nan_count == self.CHECK_STRAIGHT_LASER_DATA:
                     self.publish_movement(self.CMD_LINEAR)
                     continue
 
                 # Filter out nan's in order to calc average distance
-                data = filter(lambda x: isnan(x) is False, compressed_msg)
+                data = filter(lambda x: np.isnan(x) is False, compressed_msg)
                 avg = sum(data) / float(len(data))
                 if avg > self.SAVE_DISTANCE:
+                    # determine desired orientation
+                    self.turn_to_goal()
                     self.publish_movement(self.CMD_LINEAR)
                     continue
 
                 if avg < self.SAVE_DISTANCE:
                     break
 
+                # start moving straight till running into an obstacle/wall
+
         except KeyboardInterrupt:
             rospy.logwarn("received KeyboardInterrupt, shutting down maze runner...")
             return False
+
+    def turn_to_goal(self):
+        """
+        TODO
+        :return:
+        """
+        origin = self.msg_pose
+        # calculate starting position; convert quaternion to euler first
+        orientations = [self.msg_pose.orientation.x, self.msg_pose.orientation.y,
+                        self.msg_pose.orientation.z, self.msg_pose.orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientations)
+        helping_angle = yaw
+        starting_point = [origin.position.x, origin.position.y]
+        goal_point = [self.goal.x, self.goal.y]
+        angle1 = np.arctan(*starting_point[::-1])
+        angle2 = np.arctan(*goal_point[::-1])
+        goal_angle = np.rad2deg((angle1 - angle2) % (2 * np.pi))
 
     def circle_detection(self):
         """
